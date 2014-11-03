eec000d0969f5aaa6c082052a6f3b37dd7f78862
SirTomOfCanada <tedarlin@nickelback.cse.buffalo.edu>
2014-11-03 16:45:13 -0500

    Moar tests

--- AwesomeTikal/src/tests/ReferenceTests.java
+++ AwesomeTikal/src/tests/ReferenceTests.java
@@ -29,14 +29,14 @@ public class ReferenceTests {
 	public void ownerTest01(){
 		Player player01 = new Player("Mike", "Yellow");
 		Player player02 = new Player("Tom", "Blue");
-		Temple tile01 = new Temple(1);
-		tile01.addPiece(player01, new Piece(player01));
-		tile01.addPiece(player01, new Piece(player01));
-		tile01.addPiece(player02, new Piece(player02));
-		tile01.addPiece(player02, new Piece(player02));
-		tile01.addPiece(player02, new Piece(player02));
+		Temple temple01 = new Temple(1);
+		temple01.addPiece(player01, new Piece(player01));
+		temple01.addPiece(player01, new Piece(player01));
+		temple01.addPiece(player02, new Piece(player02));
+		temple01.addPiece(player02, new Piece(player02));
+		temple01.addPiece(player02, new Piece(player02));
 		try {
-			assertTrue(tile01.owner().equals(player02));
+			assertTrue(temple01.owner().equals(player02));
 		} catch (UnoccupiedTileException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -47,14 +47,31 @@ public class ReferenceTests {
 	public void ownerTest02(){
 		Player player01 = new Player("Mike", "Yellow");
 		Player player02 = new Player("Tom", "Blue");
-		Temple tile01 = new Temple(1);
-	
-		assertTrue(tile01.isUnoccupied());
-		
+		Temple temple01 = new Temple(1);
+		temple01.addPiece(player01, new Piece(player01));
+		temple01.addPiece(player01, new Piece(player01));
+		temple01.addPiece(player02, new Piece(player02));
+		temple01.addPiece(player02, new Piece(player02));
+		try {
+			assertTrue(temple01.owner().equals(player02));
+		} catch (UnoccupiedTileException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
 	public void ownerTest03(){
+		Player player01 = new Player("Mike", "Yellow");
+		Player player02 = new Player("Tom", "Blue");
+		Temple temple01 = new Temple(1);
+	
+		assertTrue(temple01.isUnoccupied());
+		
+	}
+	
+	@Test
+	public void ownerTest04(){
 		Player player01 = new Player("Mike", "Yellow");
 		Player player02 = new Player("Tom", "Blue");
 		Temple tile01 = new Temple(1);
