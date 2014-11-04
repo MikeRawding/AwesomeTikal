cc874636d04d4575cc9efe05aea415004dd09010
mrawding <mrawding@styx.cse.buffalo.edu>
2014-11-03 20:27:54 -0500

    GUI is getting closer very slowly and its painful

--- AwesomeTikal/src/code/Board.java
+++ AwesomeTikal/src/code/Board.java
@@ -2,49 +2,57 @@ package code;
 //Would this work for us?
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.LayoutManager;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Board{
 
+	
 	public Board(){
 		JButton[][] grid = new JButton[6][6];
-		JFrame frame = new JFrame("Welcome to Tikal");
-		frame.setSize(new Dimension(1000,800));
+		JFrame frame = new JFrame("Welcome to Tikal (the game of stealing from the natives)");
+		
 		
 		JPanel[] columns = new JPanel[6];
 		for(int i = 0; i < columns.length; i++){
 			columns[i] = new JPanel();
-			columns[i].setLayout(new GridLayout(6,1));
+			columns[i].setLayout(new BoxLayout(columns[i],1));
 		}
+		
 		
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel);
 		panel.setLayout(new GridLayout(1,6));
-		for(int y=0; y<columns.length; y++){
-			panel.add(columns[y]);
-			if(y%2==0){
-				for(int x=0; x<5; x++){
+		for(int x=0; x<columns.length; x++){
+			panel.add(columns[x]);
+			if(x%2==0){
+				columns[x].add(new JPanel());
+				for(int y=0; y<5; y++){
 					grid[x][y]=new JButton("" +x+ "" +y+ ""); //creates new button     
-					columns[y].add(grid[x][y]); //adds button to grid
+					columns[x].add(grid[x][y]); //adds button to grid
 				}
 			}
 			else{
-				for(int x=0; x<6; x++){
+				for(int y=0; y<6; y++){
 					grid[x][y]=new JButton("" +x+ "" +y+ ""); //creates new button     
-					columns[y].add(grid[x][y]); //adds button to grid
+					columns[x].add(grid[x][y]); //adds button to grid
 				}
 			}
 		}
 
 		frame.add(panel);
 		//button.setMnemonic('Q'); // associate hotkey to button
-
+		frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
